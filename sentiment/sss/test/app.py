8057f07d3709675731eca1584f39938706a0e4ad
Chualchai Apichatitiworn <whatbest@MacBook-Pro-khxng-Chualchai.local>
2023-02-09 23:35:45 +0700

    แ

--- sentiment/sss/test/app.py
+++ sentiment/sss/test/app.py
@@ -1,33 +1,96 @@
-from pywebio import start_server, config
-from pywebio.output import *
-from pywebio.pin import *
+# Import the libraries.
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
 
-@config(theme='dark')
-def main():
-    put_grid([
-        [span(put_markdown('## Section A'), col=3)],
-        [put_markdown('### Chart 1'), put_markdown('### Chart 2'), put_markdown('### Chart 3')],
-        [put_markdown(md), put_scope('1-2'), put_scope('1-3')],
-        [span(put_markdown('## Section B'), col=2, row=1), put_markdown('## Section C')],
-        [span(put_row([
-                put_select('x', help_text='X column', options=['a', 'b']),
-                put_select('y', help_text='Y column', options=['x', 'y']),
-                ]), col=2, row=1),
-            None, 
-        ],
-        [span(put_image(img_link), col=2, row=1), put_scope('2-3')],
-    ], cell_widths='33% 33% 33%')
+# Specify title and logo for the webpage.
+st.set_page_config(page_title='Nvidia Stock Prices', 
+page_icon='https://cdn.freebiesupply.com/logos/thumbs/1x/nvidia-logo.png', layout="wide")
 
-    with use_scope('1-2'):
-        put_html(gdp_chart('animation'))
-        
-    with use_scope('1-3'):
-        put_html(gdp_chart('geo'))
-        
-    with use_scope('2-3'):
-        put_markdown(md)
-        put_input('something', label='input something to show as a toast message')
-        put_button('submit', onclick=lambda: toast(pin.something))
+# Add social media tags and links to the web page.
+"""
+[![Star](https://img.shields.io/badge/Medium-12100E?style=for-the-badge&logo=medium&logoColor=white)](https://medium.com/@dniggl)
+[![Follow](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/dennisniggl)
+[![Follow](https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/DennisNiggl)
 
-if __name__ == '__main__':
-    start_server(main, port=8080, debug=True)
+# Nvidia's Stock Performance
+
+"""
+
+# Add a sidebar to the web page. 
+st.markdown('---')
+# Sidebar Configuration
+st.sidebar.image('https://cdn.freebiesupply.com/logos/thumbs/1x/nvidia-logo.png', width=200)
+st.sidebar.markdown('# Nvidia Stock Price Analysis')
+st.sidebar.markdown('Nvidia is a global leader in artificial intelligence hardware and software.')
+st.sidebar.markdown('Stock Data from 2019 thru 2021')
+st.sidebar.markdown('You can visualise Nvidia \'s Stock Prices Trends and Patterns over a given time span.') 
+
+st.sidebar.markdown('---')
+st.sidebar.write('Developed by Dennis Niggl')
+st.sidebar.write('Contact at dniggl@cox.net')
+
+# Import the dataset into a dataframe. 
+df = pd.read_csv('NVidia_stock_history.csv')
+
+# Delete rows where date is before 1/1/2019.
+df['Date'] = pd.to_datetime(df['Date'])
+df = df[~(df['Date'] < '2019-01-01')]
+
+# Reset the index to the Date column. 
+df['Date'] = pd.to_datetime(df['Date'],format='%Y/%m/%d')
+df.reset_index(drop=True,inplace=True)
+df.set_index('Date',inplace=True)
+
+# Display the Data in the App.
+st.subheader('Looking at the Data')
+st.dataframe(df.head())
+
+# Display statistical information on the dataset.
+st.subheader('Statistical Info about the Data')
+st.write(df.describe())
+
+# Selection for a specific time frame.
+st.subheader('Select a Date Range')
+df_select = df
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.write('Select a Start Date')
+    start_date = st.date_input('Start Date',min_value= datetime.date(2019,1,2),max_value=datetime.date(2021,11,12),value=datetime.date(2019,1,2))
+
+with col2:    
+    st.write('Select an End Date')
+    end_date = st.date_input('End Date',min_value=datetime.date(1999,1,22),max_value=datetime.date(2021,11,12),value=datetime.date(2021,11,12))
+
+if(start_date != None or end_date != None):
+    if(start_date < end_date):
+        df_select = df[start_date:end_date]
+    else:
+        st.warning("Invalid Date Range - Re-enter Dates")
+
+# Graphs and charts for selected date range.
+
+# Open & Close Prices.  
+st.subheader("Open & Close Prices for Nvidia Stock")
+st.markdown("\n\n")
+st.line_chart(df_select[['Open','Close']])
+
+# High and Low Values. 
+st.subheader("High and Low Prices for Nvidia Stock")
+st.markdown("\n\n")
+st.line_chart(df_select[['High', 'Low']])
+
+# Volume of Stock Traded.
+st.subheader("Volumn Traded for Nvidia Stock")
+st.markdown("\n\n")
+st.bar_chart(df_select['Volume'])
+
+# Moving average from 50 days to 250 days.
+st.subheader('Moving Averages of Open and Closing Stock Prices')
+movevavg_len = st.slider('Select the number of days for Moving Averages',min_value=0,max_value=250,value=50)
+moveavg_oc =  df_select[['Open','Close']].rolling(50).mean()
+st.line_chart(moveavg_oc)
